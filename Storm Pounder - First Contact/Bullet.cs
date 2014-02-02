370c44d5ea07ba6f7d5d1bc62cbf63bc9a2452ca
Emil Gedda <emil.gedda@emilgedda.se>
2014-02-02 17:03:24 +0100

    Bullet testing

--- Storm Pounder - First Contact/Bullet.cs
+++ Storm Pounder - First Contact/Bullet.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace Storm_Pounder___First_Contact
     class Bullet : PhysicalObject
     {
         const float BulletSpeed = 15F;
+        private float health = 100;
+        private Vector2 spawnPoint;
+        public float Health { get { return health; } set { health = value; } }
+        private float maxDistance = 100;
+        public float MaxDistance { get { return maxDistance; } set { maxDistance = value; } } 
+
         public Bullet(Texture2D texture, float X, float Y)
             : base(texture, X, Y, 0, BulletSpeed)
         {
@@ -17,7 +24,7 @@ namespace Storm_Pounder___First_Contact
         public void Update()
         {
             vector.Y -= speed.Y;
-            if (vector.Y + texture.Height < 0)
+            if (vector.Y + texture.Height < 0 || health <= 0)
                 IsAlive = false;
         }
     }
